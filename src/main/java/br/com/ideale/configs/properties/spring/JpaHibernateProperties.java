ab2c2efc74048c624f649ae954bd32f1fa6a5c80
Bruno Fontes <brunobfonttes@gmail.com>
2023-06-17 17:31:45 -0300

    feat: init project

---
+++ src/main/java/br/com/ideale/configs/properties/spring/JpaHibernateProperties.java
@@ -0,0 +1,18 @@
+package br.com.ideale.configs.properties.spring;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@ConfigurationProperties(prefix="spring.jpa.hibernate")
+public class JpaHibernateProperties {
+
+    @NotBlank
+    private final String ddlAuto;
+
+    public JpaHibernateProperties(String ddlAuto) {
+        this.ddlAuto = ddlAuto;
+    }
+
+}
---
+++ src/main/java/br/com/ideale/exception/GlobalExceptionHandler.java
@@ -0,0 +1,94 @@
+package br.com.ideale.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import br.com.ideale.configs.properties.AppProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    public static final String TRACE = "trace";
+
+    @Autowired
+    private AppProperties appProperties;
+
+    private boolean printStackTrace(){
+        return this.appProperties.getTrace();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected  Map<String, List<String>> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, WebRequest request) {
+
+        Map<String, List<String>> result = new HashMap<>();
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        result.put("errors", errors);
+
+        return result;
+    }
+    @ResponseBody
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public  Map<String, List<String>> constraintViolationException(ConstraintViolationException ex, WebRequest request) {
+        List<String> errors = new ArrayList<>();
+
+        ex.getConstraintViolations().forEach(cv -> errors.add(cv.getMessage()));
+
+        Map<String, List<String>> result = new HashMap<>();
+
+        result.put("errors", errors);
+
+        return result;
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(Exception exception,
+                                                      HttpStatus httpStatus,
+                                                      WebRequest request) {
+        return buildErrorResponse(exception, exception.getMessage(), httpStatus, request);
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(Exception exception,
+                                                      String message,
+                                                      HttpStatus httpStatus,
+                                                      WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), message);
+        if (this.printStackTrace() && isTraceOn(request)) {
+            errorResponse.setStackTrace(ExceptionUtils.getStackTrace(exception));
+        }
+        return ResponseEntity.status(httpStatus).body(errorResponse);
+    }
+
+    private boolean isTraceOn(WebRequest request) {
+        String[] value = request.getParameterValues(TRACE);
+        return Objects.nonNull(value)
+                && value.length > 0
+                && value[0].contentEquals("true");
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Object> handleAllUncaughtException(Exception exception, WebRequest request) {
+        return buildErrorResponse(exception, "Unknown error occurred", HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+}
