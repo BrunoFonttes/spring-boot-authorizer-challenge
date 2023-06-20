793859235834550bea0165f06860698e27306142
Bruno Fontes <brunobfonttes@gmail.com>
2023-06-19 23:20:16 -0300

    refactors: refactors card implementation

--- src/main/java/br/com/ideale/model/Card.java
+++ src/main/java/br/com/ideale/model/Card.java
@@ -3,68 +3,71 @@ package br.com.ideale.model;
 import br.com.ideale.exception.InsufficienteBalanceException;
 import br.com.ideale.exception.InvalidPasswordException;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import jakarta.xml.bind.DatatypeConverter;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.SneakyThrows;
 import org.hibernate.annotations.OptimisticLockType;
 import org.hibernate.annotations.OptimisticLocking;
-
 import java.security.MessageDigest;
 
 @Entity
-@Getter
-@Setter
 @OptimisticLocking(type = OptimisticLockType.VERSION)
 public class Card {
 
     @Id
+    @Getter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "The card number is required.")
     @Pattern(regexp = "^[0-9]*$")
     @Size(min = 13, max = 16)
-    @Column(unique=true)
+    @Column(unique = true)
+    @Getter
     private String cardNumber;
 
     @NotBlank(message = "The card password is required.")
+    @Getter
     private String hashedPassword;
 
     @Min(0)
+    @Getter
     private Float balance;
 
     @Version
+    @Getter
     private long version;
 
-    public Card(){}
-
-    @SneakyThrows
-    private String getHashedPassword(String password)  {
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        md.update(password.getBytes());
-        byte[] digest = md.digest();
-       return DatatypeConverter
-                .printHexBinary(digest).toUpperCase();
+    public Card() {
     }
+
     public Card(String cardNumber, String hashedPassword) {
         this.cardNumber = cardNumber;
         this.hashedPassword = this.getHashedPassword(hashedPassword);
         this.balance = (float) 500;
     }
 
-    private void authorize(String password){
-        if(!this.hashedPassword.equals(getHashedPassword(password))) {
-            throw new InvalidPasswordException();
-        }
+    @SneakyThrows
+    private String getHashedPassword(String password) {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(password.getBytes());
+        byte[] digest = md.digest();
+        return DatatypeConverter
+                .printHexBinary(digest).toUpperCase();
     }
 
-   public void consumeBalance(String password , @Min(0) Float value) throws InsufficienteBalanceException{
-        authorize(password);
-        if (this.balance-value < 0){
-            throw new InsufficienteBalanceException();
+    public class AuthorizedOperations {
+        public AuthorizedOperations(String password) {
+            assert Card.this.hashedPassword.equals(getHashedPassword(password)): new InvalidPasswordException();
         }
-        this.setBalance(this.balance - value);
+
+        public void consumeBalance(@Min(0) Float value) throws InsufficienteBalanceException {
+           assert Card.this.balance - value > 0: new InsufficienteBalanceException();
+            Card.this.setBalance(Card.this.balance - value);
+        }
     }
 }
